594b55896990c33843b9de6437210e30fb7040bd
0xc000007b <rammleid123@gmail.com>
2023-02-02 17:02:26 +0300

    add rational calc func

--- handlers/math/Calculator.py
+++ handlers/math/Calculator.py
@@ -1,17 +1,19 @@
 from menus.Menus import *
-
+from fractions import Fraction
 
 @dp.message_handler(Text(startswith = 'num_'))
 async def parse_number(message: types.Message):
     nums = message.text.split('num_')
     nms = str(nums[1]).split(' ')
 
-    num = nms[0]
+    num = complex(nms[0])
     char = nms[1]
-    numTwo = nms[2]
-    if num != 0 and numTwo != 0:
+    numTwo = complex(nms[2])
+    if complex(num) != 0 and complex(numTwo) != 0:
         ans = calculate(num, char, numTwo)
-        await  message.answer(f'Ответ: {complex(ans)}')
+
+        await  message.answer(f'Ответ после произведения операции над комплексными числами: {complex(ans)}\n'
+                              )
     add_to_csv(str(ans))
 
 
@@ -28,9 +30,17 @@ def calculate(a, b, c):
             return  a - c
 
 def add_to_csv(answer):
-    with open('db.csv', 'w') as f:
-        f.write(answer)
+    with open('db.csv', 'a') as f:
+        f.write('\n'+answer)
 
 
+@dp.message_handler(Text(startswith = 'rac'))
+async def calc_rac(message: types.Message):
+    msg = message.text.split('rac_')
+    num = msg[1].split(' ')[0]
+    numTwo =  msg[1].split(' ')[1]
+    ans = Fraction(int(num), int(numTwo))
+    add_to_csv(str(ans))
+    await message.answer(f'Ответ: {ans}')
 
 
